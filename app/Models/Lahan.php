d35a90c6b807df6530f9d053d3019f5662ceea72
AliMasykur10 <alimasykur2001@gmail.com>
2026-08-14 11:29:57 +0700

    Tambah model Lahan, Partner, dan PartnerAgreement beserta relasinya

---
+++ app/Models/Lahan.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lahan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'luas_panjang_m',
+        'luas_lebar_m',
+        'jarak_tanam_m',
+        'jarak_pagar_m',
+        'estimasi_jumlah_pohon',
+        'fase_saat_ini',
+    ];
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function partnerAgreements()
+    {
+        return $this->hasMany(PartnerAgreement::class);
+    }
+
+    public function activeAgreement()
+    {
+        return $this->hasOne(PartnerAgreement::class)->where('is_active', true);
+    }
+
+    public function phaseLogs()
+    {
+        return $this->hasMany(LahanPhaseLog::class);
+    }
+
+    public function panenCycles()
+    {
+        return $this->hasMany(PanenCycle::class);
+    }
+
+    public function progressLogs()
+    {
+        return $this->hasMany(ProgressLog::class);
+    }
+
+    public function troubleReports()
+    {
+        return $this->hasMany(TroubleReport::class);
+    }
+
+    public function schedules()
+    {
+        return $this->hasMany(Schedule::class);
+    }
+
+    public function assetAllocations()
+    {
+        return $this->hasMany(AssetAllocation::class);
+    }
+}
---
+++ app/Models/PartnerAgreement.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PartnerAgreement extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'partner_id',
+        'lahan_id',
+        'skema',
+        'nominal_sewa',
+        'persentase_bagi_hasil',
+        'tanggal_mulai',
+        'tanggal_berakhir',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'tanggal_mulai' => 'date',
+        'tanggal_berakhir' => 'date',
+        'is_active' => 'boolean',
+    ];
+
+    public function partner()
+    {
+        return $this->belongsTo(Partner::class);
+    }
+
+    public function lahan()
+    {
+        return $this->belongsTo(Lahan::class);
+    }
+}
